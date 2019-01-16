7fa05fb5a6ac88025d441b3f9aed6729fdab0a31
captainwong <1281261856@qq.com>
2019-01-16 16:02:24 +0800

    add md5

--- README.md
+++ README.md
@@ -21,6 +21,12 @@ TechPowerUp GPU-Z SharedMemory Reader.
 
 Based on [GPU-Z Shared Memory Layout](https://www.techpowerup.com/forums/threads/gpu-z-shared-memory-layout.65258/)
 
+## md5
+
+Command line tool based on cryptopp563 that shows md5 output of input.
+
+Usage:  `md5 [source-string]`
+
 ## putty_launcher_MFC
 
 ![img](https://github.com/captainwong/Tools/blob/master/putty_launcher_MFC/putty_launcher_MFC.png?raw=true)
---
+++ md5/md5.cpp
@@ -0,0 +1,46 @@
+// md5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <cassert>
+
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+#include <cryptopp/md5.h>
+#include <cryptopp/hex.h>
+
+#ifdef _DEBUG
+#pragma comment(lib, "E:\\dev_libs\\cryptopp563\\cryptopp\\Win32\\Output\\DebugMDD\\cryptlib.lib")
+#else
+#pragma comment(lib, "E:\\dev_libs\\cryptopp563\\cryptopp\\Win32\\Output\\ReleaseMd\\cryptlib.lib")
+#endif // _DEBUG
+
+
+std::string md5(const std::string& message)
+{
+	byte digest[CryptoPP::Weak::MD5::DIGESTSIZE];
+
+	CryptoPP::Weak::MD5 hash;
+	hash.CalculateDigest(digest, (const byte*)message.c_str(), message.length());
+
+	CryptoPP::HexEncoder encoder(0, false);
+	std::string output;
+
+	encoder.Attach(new CryptoPP::StringSink(output));
+	encoder.Put(digest, sizeof(digest));
+	encoder.MessageEnd();
+
+	return output;
+}
+
+int main(int argc, char** argv)
+{
+	assert(md5("") == "d41d8cd98f00b204e9800998ecf8427e");
+	assert(md5("The quick brown fox jumps over the lazy dog") == "9e107d9d372bb6826bd81d3542a419d6");
+	assert(md5("The quick brown fox jumps over the lazy dog.") == "e4d909c290d0fb1ca068ffaddf22cbd0");
+
+	std::string message = argc == 2 ? argv[1] : "";
+	std::cout << md5(message) << std::endl;
+
+	return 0;
+}
