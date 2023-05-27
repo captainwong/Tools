f23404c20d62eb9da082b44a4bde5b3417faa679
i7-8700 <1281261856@qq.com>
2023-05-27 18:35:36 +0800

    fix pwdgen

--- pwdgen/pwdgen.cpp
+++ pwdgen/pwdgen.cpp
@@ -10,8 +10,8 @@ void usage(const char* argv0)
 		   "  use special chars: default 0\n", argv0);
 }
 
-const char noSpecialChars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-const char withSpecialChars[] = R"(abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()_+-=[]{};:\|,./<>?)";
+const char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+const char specialChars[] = R"(~!@#$%^&*()_+-=[]{};:\|,./<>?)";
 
 
 int main(int argc, char** argv)
@@ -21,24 +21,42 @@ int main(int argc, char** argv)
 
 	if (argc > 1) {
 		len = atoi(argv[1]);
-		if (len < 1) { usage(argv[0]); return 0; }
+		if (len < 6) { usage(argv[0]); return 0; }
 	}
 	if (argc > 2) {
-		useSpecialChars = atoi(argv[1]) == 1;
+		useSpecialChars = atoi(argv[2]) == 1;
 	}
 
-	const char* source = withSpecialChars;
-	size_t source_size = sizeof(withSpecialChars) - 2;
-	if (!useSpecialChars) {
+	const char* source = alphabet;
+	size_t source_size = sizeof(alphabet) - 2;
+	/*if (!useSpecialChars) {
 		source = noSpecialChars;
 		source_size = sizeof(noSpecialChars) - 2;
-	}
+	}*/
 
 	std::string pwd;
 	auto rgn = seeded_random_engine();
 	std::uniform_int_distribution<size_t> dist(0, source_size);
 	for (int i = 0; i < len; i++) {
 		pwd += source[dist(rgn)];
+	}
+
+	if (useSpecialChars) {
+		int slen = len / 8;
+		if (slen == 0)
+			slen = len / 4;
+		if (slen == 0)
+			slen = len / 2;
+		if (slen == 0)
+			slen = 1;
+
+		size_t s_source_size = sizeof(specialChars) - 2;
+		std::uniform_int_distribution<size_t> s_dist(0, s_source_size);
+		std::uniform_int_distribution<size_t> p_dist(0, len);
+
+		for (int i = 0; i < slen; i++) {
+			pwd[p_dist(rgn)] = specialChars[s_dist(rgn)];
+		}
 	}
 	
 	printf("%s\n", pwd.data());
